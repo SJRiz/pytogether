3828882bfbc439cfb9db055b18d8bd44c87ce683
SJRiz <rizvij006@gmail.com>
2025-08-16 22:50:49 -0400

    created project model

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from usergroups.models import Group
 
-# Create your models here.
+class Project(models.Model):
+    project_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='projects')
--- backend/usergroups/admin.py
+++ backend/usergroups/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Group
 
-# Register your models here.
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('id', 'group_name', 'owner', 'access_code')
+    search_fields = ('group_name', 'owner__email', 'access_code')
+    filter_horizontal = ('group_members',)  # makes ManyToMany field easier to manage
