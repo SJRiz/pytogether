01d7c9bc642ab70126308fbbaae5164dd06cf7c2
SJRiz <rizvij006@gmail.com>
2025-08-17 18:26:28 -0400

    dockerized backend

--- backend/projects/admin.py
+++ backend/projects/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("id", "project_name", "group", "created_at")
+    list_filter = ("group", "created_at")
+    search_fields = ("project_name", "group__name")
+    ordering = ("-created_at",)
