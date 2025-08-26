0ffccf6ee138e1153ebb887fce9cc2d5d4f21ece
Jawad <rizvij006@gmail.com>
2025-08-26 02:14:23 -0400

    cleaned up tasks, removed unneeded variables

--- backend/codes/tasks.py
+++ backend/codes/tasks.py
@@ -1,17 +1,11 @@
-import os
-import redis
 from celery import shared_task
 
-from .redis_helpers import persist_ydoc_to_db, ACTIVE_PROJECTS_SET
-
-
-REDIS_URL = os.environ.get("REDIS_URL", "redis://redis:6379/0")
-redis_client = redis.Redis.from_url(REDIS_URL)
+from .redis_helpers import persist_ydoc_to_db, SYNC_REDIS, ACTIVE_PROJECTS_SET
 
 @shared_task
 def snapshot_active_projects():
     """Loop through all active projects and save their code to the db"""
-    project_ids = redis_client.smembers(ACTIVE_PROJECTS_SET)
+    project_ids = SYNC_REDIS.smembers(ACTIVE_PROJECTS_SET)
     processed = []
 
     for id in project_ids:
@@ -21,7 +15,7 @@ def snapshot_active_projects():
             continue
 
         # A lock isn't really necessary yet, but it's nice to have when we scale
-        lock = redis_client.lock(f"lock:project_persist:{pid}", timeout=10)
+        lock = SYNC_REDIS.lock(f"lock:project_persist:{pid}", timeout=10)
         got = lock.acquire(blocking=False)
         if not got:
             continue
@@ -33,5 +27,6 @@ def snapshot_active_projects():
                 lock.release()
             except Exception:
                 pass
+            
     return processed
     
