c9cca18a8ce86c52154d178021ae2b43e4134898
Jawad <rizvij006@gmail.com>
2025-09-19 18:42:32 -0400

    fixed incorrect import

--- backend/codes/management/commands/cleanup_codes.py
+++ backend/codes/management/commands/cleanup_codes.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
-from backend.codes.tasks import snapshot_active_projects
-from backend.codes.redis_helpers import SYNC_REDIS
+from codes.tasks import snapshot_active_projects
+from codes.redis_helpers import SYNC_REDIS
 
 class Command(BaseCommand):
     help = "Snapshot all active projects and flush Redis"
