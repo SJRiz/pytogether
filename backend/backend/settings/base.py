0be1ba7e41035e1bd092156b349f505791733182
SJRiz <rizvij006@gmail.com>
2025-10-17 22:30:56 -0400

    increased access token lifetime

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -134,7 +134,7 @@ REST_FRAMEWORK = {
 
 # JWT stuff
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 
