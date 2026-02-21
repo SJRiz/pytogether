41e13dd8a0781b520ba23d5f8ca144da7abf4c2a
SJRiz <rizvij006@gmail.com>
2026-02-20 23:54:03 -0500

    umami will only track analytics on the actual pytogether site

--- frontend/reactapp/src/main.jsx
+++ frontend/reactapp/src/main.jsx
@@ -5,6 +5,17 @@ import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App.jsx";
 
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+
+if (apiBaseUrl === "https://api.pytogether.org") {
+  const script = document.createElement('script');
+  script.async = true;
+  script.defer = true;
+  script.src = "https://cloud.umami.is/script.js";
+  script.setAttribute('data-website-id', "8606b76d-95d9-486a-97b1-13a91b5263fd");
+  document.head.appendChild(script);
+}
+
 createRoot(document.getElementById("root")).render(
   <HelmetProvider>
     <BrowserRouter>
